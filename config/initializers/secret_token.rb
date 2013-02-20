903418093232edfa909e59457b50857e6b03ed23
sandra <sandra@sandras-MacBook-Air.local>
2013-02-20 02:06:14 -0300

    Generated the rails 3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fashion::Application.config.secret_token = 'd291ec68b547e71a091af434d53c3a2ad0babc662fca5a3bc0504af0f1fe5d994a755bbc4b5987be48f396207ec206d67442546bb0405e9b9fa042db85b03e74'
